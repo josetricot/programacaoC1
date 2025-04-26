0c68822d8844da86c6f12b7d67e1b8f486d81259
josetricot <josetricot@gmail.com>
2025-04-26 19:46:51 -0300

    Update Simulado Questão 3 - Professor.c

--- Simulado Questão 3 - Professor.c
+++ Simulado Questão 3 - Professor.c
@@ -1,6 +1,35 @@
 /******************************************************************************
 
-Simulado Questão 3 - Professor
+Simulado - Questão 3 (PROFESSOR)
+Programador: José Athaulpa Kolesny Tricot
+
+Em uma eleição existem 4 candidatos à vereador e 3 candidatos à
+prefeitura. O programa deve realizar a leitura do voto para vereador e para
+prefeito.
+Os votos são informados através de códigos.
+Os dados utilizados para a contagem dos votos obedecem à seguinte codificação:
+Para vereador os códigos são: 66666 para candidato A; 77777 para candidato B;
+88888 para candidato C; 99999 para candidato D
+Para prefeito os códigos são: 99 para candidato X , 88 para Candidato Y; 77 para
+candidato Z;
+111 = voto em branco (para ambas as votações);
+-Outro valor: Voto Nulo;
+Elabore um algoritmo que realize a leitura de ambos os votos (vereador e depois
+para prefeito) leia os códigos do candidato que receberá o voto. O programa
+deverá ler 7 votos (7 para vereador e 7 para prefeito) alternadamente.
+Calcule e escreva:
+-total de votos para cada candidato e seu respectivo percentual de votos sobre os
+votos válidos*; (em cada uma das duas eleições)
+-total de votos nulos; (em cada uma das duas eleições)
+-total de votos em branco; (em cada uma das duas eleições)
+Votos válidos são todos os votos - nulos.
+Lembre-se: os votos em branco e nulos podem ser diferentes nas duas
+eleições.
+a - Leitura correta dos dados;
+b - Estrutura de repetição ;
+c - Cálculo correto da Contagem de Votos ;
+Nota: o Programa deve conter comentários e o nome do autor.
+Ao FINAL deve ser exibido o nome do programador.
 
 *******************************************************************************/
 #include <stdio.h>
