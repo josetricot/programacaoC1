6b8b7d9b77ce72dcddec0df9fd0b7fe7dd0db4a7
josetricot <josetricot@gmail.com>
2025-06-26 21:21:51 -0300

    Update 6.5 - Atividade Prática - Desafio - 6.c

--- 6.5 - Atividade Prática - Desafio - 6.c
+++ 6.5 - Atividade Prática - Desafio - 6.c
@@ -1,12 +1,3 @@
-Desafio 6: Verificação de Ano Bissexto 
-Descrição: O programa deve receber um ano e informar se ele é bissexto. 
-Dica: Um ano é bissexto se for divisível por 4 e não for divisível por 100, a menos que 
-também seja divisível por 400. 
-Exemplo de Entrada: 
-Digite um ano: 2024 
-Saída Esperada: 
-O ano 2024 é bissexto. 
-
 /*### **Desafio 6: Verificação de Ano Bissexto**  
 **Descrição:** O programa deve receber um ano e informar se ele é bissexto.  
 **Dica:** Um ano é bissexto se for divisível por 4 e não for divisível por 100,
