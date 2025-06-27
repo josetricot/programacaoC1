b50c3a75ab4ff6a8d9ca328d2784ebf5bf7f8273
josetricot <josetricot@gmail.com>
2025-06-26 21:15:11 -0300

    Update 6.5 - Atividade Prática - Desafio - 11.c

--- 6.5 - Atividade Prática - Desafio - 11.c
+++ 6.5 - Atividade Prática - Desafio - 11.c
@@ -1,4 +1,4 @@
-Desafio 11: Conversão de Moeda 
+/*Desafio 11: Conversão de Moeda 
 Descrição: O programa deve converter um valor em reais (BRL) para dólares (USD), 
 considerando uma taxa fixa de câmbio. 
 Dica: Multiplique o valor em BRL pela cotação informada. 
@@ -6,4 +6,4 @@ Exemplo de Entrada:
 Digite o valor em BRL: 100 
 Digite a cotação do dólar: 5.10 
 Saída Esperada: 
-100 BRL equivalem a 19.61 USD
+100 BRL equivalem a 19.61 USD/*
