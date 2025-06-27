39dea3ba52fc6ec9d21c2f283728d448b535d4c9
josetricot <josetricot@gmail.com>
2025-06-26 21:14:39 -0300

    Update and rename 6.5 - Atividade Prática - Desafio - 12 to 6.5 - Atividade Prática - Desafio - 12.c

--- 6.5 - Atividade Prática - Desafio - 12.c
+++ 6.5 - Atividade Prática - Desafio - 12.c
@@ -1,4 +1,4 @@
-Desafio 12: Cálculo do Desconto 
+/*Desafio 12: Cálculo do Desconto 
 Descrição: O programa deve calcular o preço final de um produto após um desconto 
 percentual informado pelo usuário. 
 Dica: Multiplique o valor pelo percentual de desconto e subtraia do valor original. 
@@ -6,4 +6,4 @@ Exemplo de Entrada:
 Digite o preço original: 200 
 Digite o percentual de desconto: 10 
 Saída Esperada: 
-O preço com desconto é 180.00
+O preço com desconto é 180.00*/
