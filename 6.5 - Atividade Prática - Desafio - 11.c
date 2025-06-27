b3578d298d7fbcc1d4c6a49ccdb42497b9296f4d
josetricot <josetricot@gmail.com>
2025-06-26 21:35:47 -0300

    Update 6.5 - Atividade Prática - Desafio - 11.c

--- 6.5 - Atividade Prática - Desafio - 11.c
+++ 6.5 - Atividade Prática - Desafio - 11.c
@@ -1,4 +1,6 @@
-/*Desafio 11: Conversão de Moeda 
+/******************************************************************************
+DESAFIO 11 - CONVERSÃO DE MOEDA
+
 Descrição: O programa deve converter um valor em reais (BRL) para dólares (USD), 
 considerando uma taxa fixa de câmbio. 
 Dica: Multiplique o valor em BRL pela cotação informada. 
@@ -6,4 +8,21 @@ Exemplo de Entrada:
 Digite o valor em BRL: 100 
 Digite a cotação do dólar: 5.10 
 Saída Esperada: 
-100 BRL equivalem a 19.61 USD*/
+100 BRL equivalem a 19.61 USD
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    
+    float valorReais, valorDolar, cotacaoDolar;
+    
+    
+    printf("Digite um valor em Reais: ");
+    scanf("%f", &valorReais);
+    printf("Digite a cotação do Dólar: ");
+    scanf("%f", &cotacaoDolar);
+    valorDolar=valorReais/cotacaoDolar;
+    printf("%.2f BRL equivalem a %.2f USD", valorReais, valorDolar);
+    
+    return 0;
+}
