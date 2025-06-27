ac0b7e78bec55a3221e3680d1ff353657aa8acb9
josetricot <josetricot@gmail.com>
2025-06-26 21:18:23 -0300

    Update and rename 6.5 - Atividade Prática - Desafio - 9 to 6.5 - Atividade Prática - Desafio - 9.c

--- 6.5 - Atividade Prática - Desafio - 9.c
+++ 6.5 - Atividade Prática - Desafio - 9.c
@@ -1,13 +1,3 @@
-Desafio 9: Cálculo do IMC 
-Descrição: O programa deve calcular o Índice de Massa Corporal (IMC) de uma pessoa 
-com base no peso e na altura. 
-Dica: Use a fórmula IMC = peso / (altura * altura). 
-Exemplo de Entrada: 
-Digite seu peso (kg): 70 
-Digite sua altura (m): 1.75 
-Saída Esperada: 
-Seu IMC é 22.86 
-
 /*### **Desafio 9: Cálculo do IMC**  
 **Descrição:** O programa deve calcular o Índice de Massa Corporal (IMC)
 de uma pessoa com base no peso e na altura.  
