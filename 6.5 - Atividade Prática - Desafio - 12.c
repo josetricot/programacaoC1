3403b9a2ee1a33eed32301da232370bf424de33e
josetricot <josetricot@gmail.com>
2025-06-26 22:13:50 -0300

    Update 6.5 - Atividade Prática - Desafio - 12.c

--- 6.5 - Atividade Prática - Desafio - 12.c
+++ 6.5 - Atividade Prática - Desafio - 12.c
@@ -1,4 +1,6 @@
-/*Desafio 12: Cálculo do Desconto 
+/******************************************************************************
+DESAFIO 12 - CÁLCULO DO DESCONTO
+
 Descrição: O programa deve calcular o preço final de um produto após um desconto 
 percentual informado pelo usuário. 
 Dica: Multiplique o valor pelo percentual de desconto e subtraia do valor original. 
@@ -6,4 +8,23 @@ Exemplo de Entrada:
 Digite o preço original: 200 
 Digite o percentual de desconto: 10 
 Saída Esperada: 
-O preço com desconto é 180.00*/
+O preço com desconto é 180.00
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    
+    float precoOriginal, percentualDesconto;
+    float precoFinal=0.00;
+    
+    printf("Digite o preço original: ");
+    scanf("%f", &precoOriginal);
+    printf("Digite o percentual de desconto: ");
+    scanf("%f", &percentualDesconto);
+    
+    precoFinal=precoOriginal-(precoOriginal*percentualDesconto/100);
+    
+    printf("O preço com desconto é R$ %.2f", precoFinal);
+
+    return 0;
+}
